64834c1257f68041f871756bc79bd7e1b9fc447c
Kevin M <shoron@gmail.com>
2017-09-21 03:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function subtract (x,y) {return x-y}
 function multiply (x,y) {return x*y}
 function divide(x,y) {return x/y}
 function inc(n) {return n+=1}
-function dec(n2) {return n2-=1}
+function dec(n) {return n-=1}
