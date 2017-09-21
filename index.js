55ea67979ac01ce86a8fd300b7824cee3fa6844b
Kevin M <shoron@gmail.com>
2017-09-21 03:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function divide(x,y) {return x/y}
 function inc(n) {return n+=1}
 function dec(n) {return n-=1}
 function makeInt (n) {return parseInt (n,10)}
+function preserveDecimal (n){return parseFloat(n,10)}
